149c615da4de05f32e5b33c5c3e1d3043dcf8c15
Diazu27 <AndresDiazu2001@gmail.com>
2022-01-21 20:34:46 -0600

    Modified product relations

--- src/models/product.js
+++ src/models/product.js
@@ -1,13 +1,33 @@
 import Sequelize from 'sequelize'
 import {connection} from '../database/connection'
+import { categoriesSchema } from './categories';
 
 
 export const productSchema = connection.define('products', {
-  Id: {
+  id: {
     type: Sequelize.INTEGER,
     primaryKey: true
   },
-  Name: {
+	name: {
     type: Sequelize.TEXT
-  }
+  },
+	categoryid: {
+    type: Sequelize.INTEGER
+  },
+	unitmeasure: {
+    type: Sequelize.TEXT
+  },
+	pricebyunit: {
+    type: Sequelize.FLOAT
+  },
 });
+
+/* 
+ -En esta relacion digo que seleccione productos
+ - Que una categoria tiene muchos productos y que tome el id como llave
+ - Un producto pertenece a una categoria, y tomo la categoryid
+
+*/
+categoriesSchema.hasMany(productSchema, {foreignKey: 'id', sourceKey: 'id'});
+productSchema.belongsTo(categoriesSchema, { foreignKey: 'categoryid', sourceKey: 'id'})
+ 
